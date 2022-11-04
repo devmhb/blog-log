56a3e47982e45bed470613f5f97416006294ed31
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-11-05 02:09:19 +0600

    authordetails

--- pages/author/[slug].js
+++ pages/author/[slug].js
@@ -1,10 +1,14 @@
 import Head from "next/head";
 import React from "react";
 import AuthorPostCard from "../../components/authorPostCard";
+import Image from "next/image";
+import Author from "../../images/Rectangle 3039.png";
+import styles from "../../styles/components/authorDetails.module.scss";
 import Layout from "../../components/layout";
 import { getAuthor, getAuthors, getCategories } from "../../service";
 
 const AuthorDetails = ({ author, categories }) => {
+  console.log(author);
   const authorInfo = author.edges[0];
   return (
     <>
@@ -13,7 +17,20 @@ const AuthorDetails = ({ author, categories }) => {
       </Head>
 
       <Layout>
-        <h1>{authorInfo.node.name}</h1>
+        <div className={styles.author_details}>
+          <div className={styles.author_img}>
+            <Image
+              src={Author}
+              height="100%"
+              width="100%"
+              alt={authorInfo.node.name}
+            />
+          </div>
+          <div className={styles.author_infos}>
+            <h1>Hi! I am {authorInfo.node.name}</h1>
+            <p className={styles.authorsbio}>{authorInfo.node.bio}</p>
+          </div>
+        </div>
 
         <div>
           {authorInfo.node.posts.map((post) => (
--- service/index.js
+++ service/index.js
@@ -116,6 +116,8 @@ export const getAuthors = async () => {
             posts {
               title
             }
+            subheading
+            subio
           }
         }
       }
