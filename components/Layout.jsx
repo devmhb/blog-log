4f911e8b737cfb7cdfe73b83ad69f637947e8e4e
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-12-30 00:08:02 +0600

    fotter problem

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from "./footer";
+import Footer from "./Footer";
 import Navbar from "./navbar";
 import { CategoriesProvider } from "./categoriesContext";
 
